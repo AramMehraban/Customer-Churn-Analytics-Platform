c7201d6d0b50aa3b4d17a35670831a71aed8d5c0
AramMehraban <268549225+AramMehraban@users.noreply.github.com>
2026-08-02 19:40:48 +0200

    Create 09_Final_views_TestSQLQuery3.sql

---
+++ SQL/analytics/09_Final_views_TestSQLQuery3.sql
@@ -0,0 +1,26 @@
+
+-- Final Test  Query
+
+SELECT * 
+FROM analytics.vw_Churn_KPI;
+
+SELECT * 
+FROM analytics.vw_Churn_By_Contract;
+
+SELECT * 
+FROM analytics.vw_Churn_By_Payment;
+
+SELECT * 
+FROM analytics.vw_Churn_By_Internet;
+
+SELECT * 
+FROM analytics.vw_Customer_Profile;
+
+SELECT * 
+FROM analytics.vw_Revenue_Analysis;
+
+SELECT * 
+FROM analytics.vw_Customer_Risk;
+
+SELECT * 
+FROM analytics.vw_Executive_Summary;
